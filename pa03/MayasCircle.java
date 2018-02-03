77fa11563e1bed63e1aba6821cc3880a9216a7cd
mayabz <33837612+mayabz@users.noreply.github.com>
2018-02-03 14:16:06 -0500

    Starting Subclass
    Starting my subclass

---
+++ pa03/MayasCircle.java
@@ -0,0 +1,13 @@
+package pa03;
+
+
+public class MayasCircle extends CircleShape{
+
+  private double vr = 0.5;
+
+  public MayasCircle(){
+    super();
+    this.color = new java.awt(0,200,50);
+
+  }
+}
