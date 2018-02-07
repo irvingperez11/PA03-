44981693163774fd06fe712cc74f4c800bce5924
mayabz <33837612+mayabz@users.noreply.github.com>
2018-02-07 12:09:39 -0500

    forgot to update init
    updating init

--- pa03/DrawingModel.java
+++ pa03/DrawingModel.java
@@ -29,7 +29,7 @@ public class DrawingModel {
 	*/
 	private void init(){
 		CircleShape c;
-		for(int i=0; i< circles.length/4; i++){
+		for(int i=0; i< circles.length/6; i++){
 			// we go to circles.length/2 as we add 2 different kinds of circles
 			// each time through this loop.. with 5 classes you'll divide by 5
 			// WHEN YOU ADD ANOTHER SUBCLASS OF CIRCLESHAPE YOU NEED TO
